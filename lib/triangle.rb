6f3d75d91b41efc11e371652aaf0ca06d607d144
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-09 15:25:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,14 +13,10 @@ class Triangle
     triangle_inequality = ((@a + @b) <= @c) || ( (@a + @c) <= @b) || ( (@b + @c) <= @a)
     zero_length =  @a == 0 || @b == 0 || @c == 0
     negative_length = @a < 0 || @b < 0 || @c < 0
-    binding.pry
+
     if triangle_inequality || zero_length || negative_length
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    end
+      raise TriangleError
+    else
       if @a == @b && @b == @c
         :equilateral
       elsif (@a == @b && @a != @c && @b != @c) || (@b == @c && @b != @a && @c != @a) || (@a == @c && @a != @b && @c != @b)
@@ -28,13 +24,12 @@ class Triangle
       else
         :scalene
       end
+    end
   end
 
 
   class TriangleError < StandardError
-    def message
-      "The sum of the lengths of any two sides of a triangle is greater than the lenght of the third side"
-    end
+
   end
 
 
