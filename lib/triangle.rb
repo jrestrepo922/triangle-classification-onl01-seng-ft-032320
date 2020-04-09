24fbb71901b13a1d5cc351af1a0591c2d5ec0017
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-09 14:47:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+
+
 class Triangle
-  # write code here
+
+  def initialize( a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    # create an if statement that takes into account the Triangle inequality theorem
+    if ((@a + @b) < @c) || ( (@a + @c) < @b) || ( (@b + @c) < @a)
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+    end
+
+      if @a == @b && @b == @c
+        :equilateral
+      elsif (@a == @b && @a != @c && @b != @c) || (@b == @c && @b != @a && @c != @a) || (@a == @c && @a != @b && @c != @b)
+        :isosceles
+      else
+        :scalene
+      end
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      "The sum of the lengths of any two sides of a triangle is greater than the lenght of the third side"
+    end
+  end
+
+
+
 end
